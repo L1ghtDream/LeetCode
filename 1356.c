c21b5988086ea8c38c037d6b23604761b64fe0b0
Voinea Radu <contact@voinearadu.com>
2023-10-30 20:20:59 +0200

    1365

--- 1356.c
+++ 1356.c
@@ -1,6 +1,64 @@
+#include <malloc.h>
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* sortByBits(int* arr, int arrSize, int* returnSize){
+int *sortByBits(int *arr, int arrSize, int *returnSize) {
+    *returnSize = arrSize;
+    int *sizes = malloc(arrSize * sizeof(int));
 
+    for (int i = 0; i < arrSize; i++) {
+        int element = arr[i];
+
+        int counter = 0;
+
+        while (element != 0) {
+            if ((element & 0b1) == 1) {
+                counter++;
+            }
+            element = element >> 1;
+        }
+
+        sizes[i] = counter;
+    }
+
+    while (1) {
+        int found = 0;
+        for (int i = 0; i < arrSize - 1; i++) {
+            if (sizes[i] > sizes[i + 1]) {
+                found = 1;
+                int tmp1 = sizes[i];
+                int tmp2 = arr[i];
+
+                sizes[i] = sizes[i + 1];
+                sizes[i + 1] = tmp1;
+
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp2;
+            }
+        }
+
+        for (int i = 0; i < arrSize - 1; i++) {
+            if (sizes[i] == sizes[i + 1]) {
+                if (arr[i] > arr[i + 1]) {
+                    found = 1;
+                    int tmp1 = sizes[i];
+                    int tmp2 = arr[i];
+
+                    sizes[i] = sizes[i + 1];
+                    sizes[i + 1] = tmp1;
+
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp2;
+                }
+            }
+        }
+
+        if (!found) {
+            break;
+        }
+    }
+
+    free(sizes);
+    return arr;
 }
--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 
 int main() {
 
+    //int tests[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    int tests[] = {1024,512,256,128,64,32,16,8,4,2,1};
+    int returnSize = 0;
+
+    int *output = sortByBits(tests, sizeof(tests) / sizeof(int), &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", output[i]);
+    }
+
 
     return 0;
 }
